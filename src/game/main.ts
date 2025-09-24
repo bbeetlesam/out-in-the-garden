ae90ec35d0505afcf977b849f55aba8d5fa7358c
bbeetlesam <samsismines@gmail.com>
2025-09-24 07:34:32 +0700

    add confs in main.ts (phaser conf)

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,25 +1,36 @@
+import { AUTO, Game, Scale } from 'phaser';
 import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import { Disclaimer } from './scenes/Disclaimer';
+import { Game as MainGame } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
 
-//  Find out more information about the Game Config at:
-//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
+    title: 'Out in the Garden',
+    url: 'https://bbeetlesam.itch.io/out-in-the-garden',
+    version: '0.0.0',
+
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 1280,
+    height: 800,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#000000ff',
     scene: [
         Boot,
         Preloader,
-        MainMenu,
+        Disclaimer,
         MainGame,
         GameOver
-    ]
+    ],
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    input: {
+        keyboard: true,
+        mouse: true,
+        windowEvents: true
+    }
 };
 
 const StartGame = (parent: string) => {
